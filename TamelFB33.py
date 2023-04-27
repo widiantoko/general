7a7c6ad5d5a52fe44bbbac1af04744442aff69c1
widiantoko <dodiwidiantoko@gmail.com>
2023-04-27 19:59:10 +0700

    update3

--- TamelFB33.py
+++ TamelFB33.py
@@ -109,8 +109,8 @@ d.kdpelanggan, d.nmpelanggan, kdproduk, kdkirim  from tkonos a
         total=sum(data_hasil)
         bold = (f"**{total}**")
 
-        pkt=list(data_kdkirim)[1]
-        doc=list(data_kdkirim)[0]
+        pkt=list(data_kdkirim)[0]
+        doc=list(data_kdkirim)[1]
 
 
         hasil2 = datafr[datafr["bln_thn"] == pilihan] 
