67d4c8648c15e8d5e1767cdbc283f11d04d8a310
widiantoko <dodiwidiantoko@gmail.com>
2025-08-14 19:59:27 +0700

    ttty

--- TamelFB33.py
+++ TamelFB33.py
@@ -739,4 +739,4 @@ if selected2=="Review Kinerja":
     with col2:
 
         filter_dp4=datapage4[datapage4['cabang'] == pil_cab]
-        st.dataframe(filter_dp4, use_container_width=True, hide_index=True,)
+        st.dataframe(filter_dp4)
