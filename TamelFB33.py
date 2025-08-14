f58775dc999b83815dd7eb7df848282c38bf09e9
widiantoko <dodiwidiantoko@gmail.com>
2025-08-14 16:51:19 +0700

    uu

--- TamelFB33.py
+++ TamelFB33.py
@@ -726,7 +726,7 @@ if selected2=="Review Kinerja":
     #st.text(datapage4.dtypes)
 
     lst_cab=datapage4["cabang"].drop_duplicates().sort_index(ascending=True)
-    st.selectbox("Pilih Cabang", lst_cab, placeholder="Cabang", key="cabang", label_visibility="collapsed")
+    st.selectbox("Pilih Cabang", lst_cab, key="cabang")
 
     #st.text(lst_cab)
 
