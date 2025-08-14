ecd75656bee54f164d3d0270392e3727ae396397
widiantoko <dodiwidiantoko@gmail.com>
2025-08-14 20:38:11 +0700

    tt

--- TamelFB33.py
+++ TamelFB33.py
@@ -726,7 +726,7 @@ if selected2=="Review Kinerja":
     st.dataframe(datapage4)
     #st.text(datapage4.dtypes)
 
-    lst_cab=datapage4["cabang"].drop_duplicates().sort_index(ascending=True)
+    #lst_cab=datapage4["cabang"].drop_duplicates().sort_index(ascending=True)
     #pilihan4=st.selectbox("Pilih Cabang", lst_cab, key="cabang")
 
     #st.text(lst_cab)
