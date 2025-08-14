186f19eb28a916b01a391694db952775ae3affbc
widiantoko <dodiwidiantoko@gmail.com>
2025-08-14 14:13:50 +0700

    nmnm

--- TamelFB33.py
+++ TamelFB33.py
@@ -718,7 +718,7 @@ if selected2=="Review Kinerja":
     
 
     datapage4=pd.DataFrame(result4)
-    st.table(datapage4.head(10))
+    st.dataframe(datapage4.head(10))
 
     #lst_cab=datapage4['cabang'].drop_duplicates().sort_index(ascending=True)
     #st.text(lst_cab)
