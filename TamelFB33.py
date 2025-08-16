e217f0922c103af50939edf08d4d49aefdef0dd2
widiantoko <dodiwidiantoko@gmail.com>
2025-08-16 12:43:02 +0700

    ui

--- TamelFB33.py
+++ TamelFB33.py
@@ -661,7 +661,7 @@ if selected2=="Review Kinerja":
     #datapage4[["normal_kg", "urgent_kg", "darat_kg", "top_urgent_kg", "outbound_kg_reg", "outbound_kg_mtx", "trip_trucking", "inbound_kg"]]=datapage4[["normal_kg", "urgent_kg", "darat_kg", "top_urgent_kg", "outbound_kg_reg", "outbound_kg_mtx", "trip_trucking", "inbound_kg"]].astype(float)
     
     st.dataframe(datapage4.head(20))
-    #st.text(datapage4.dtypes)
+    st.text(datapage4.info)
 
     #lst_cab=datapage4["cabang"].drop_duplicates().sort_index(ascending=True)
     #pilihan4=st.selectbox("Pilih Cabang", lst_cab, key="cabang")
