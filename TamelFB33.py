e6d38915d131c6078f307104248b72afd2e9234f
widiantoko <dodiwidiantoko@gmail.com>
2025-08-16 12:45:52 +0700

    okr

--- TamelFB33.py
+++ TamelFB33.py
@@ -625,7 +625,6 @@ if selected2=="Review Kinerja":
             IF(asal='CBM', 'CBH', asal) AS asal_new,
             koli, berat, kdmani, 
             IF(kdmani IN ('RAX', 'REX', 'CLT', 'SAP'), 'CBD', Kdmani) AS kdmani_new
-        
         FROM tkonos
         WHERE tanggal >= '2025-08-01' AND tanggal <= '2025-08-31'
             AND kdpelanggan NOT LIKE 'CBD18002%'
