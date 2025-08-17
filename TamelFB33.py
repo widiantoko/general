224150e1b5ee0aca8a6eb3cbdf518341a23b2c67
widiantoko <dodiwidiantoko@gmail.com>
2025-08-17 13:04:00 +0700

    uyuy

--- TamelFB33.py
+++ TamelFB33.py
@@ -655,6 +655,7 @@ if selected2=="Review Kinerja":
     datapage4[['berat']] = datapage4[['berat']].astype(float)
 
     st.text(datapage4.dtypes)
+    st.dataframe(datapage4, index=False, use_container_width=True,)
 
     #kg_tujuan=datapage4.groupby(['bulan','asal_new'])['berat'].sum().reset_index().sort_values(['asal_new'], ascending=False)
     
