64e7f38be070a4c825150d434fb50e3932680101
widiantoko <dodiwidiantoko@gmail.com>
2025-08-18 05:51:15 +0700

    change

--- TamelFB33.py
+++ TamelFB33.py
@@ -20,7 +20,7 @@ st.set_page_config(page_title="Kiriman Ke Cabang dan Agen", layout='wide')
 
 
 def init_connection():
-    return mysql.connector.connect(**st.secrets["mysql"])
+    return mysql.connector.connect(**st.secrets["config_db01"])
 
 
 conn = init_connection()
