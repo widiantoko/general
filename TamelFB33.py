7d1ffa68e29b740235b7bef33ac323e4b4ab1719
widiantoko <dodiwidiantoko@gmail.com>
2025-08-17 11:21:19 +0700

    uuiui

--- TamelFB33.py
+++ TamelFB33.py
@@ -659,12 +659,17 @@ if selected2=="Review Kinerja":
     #kg_tujuan=datapage4.groupby(['bulan','asal_new'])['berat'].sum().reset_index().sort_values(['asal_new'], ascending=False)
     
     
+    #result = df.groupby('Category').apply(lambda x: pd.Series({
+    #    'Conditional_Sum': x['Value'].where((x['Condition_Col_1'] == 'X') & 
+    #                                        (x['Condition_Col_2'] == True), 0).sum()})).reset_index()
+
+
     kg_tujuan=datapage4.groupby(['bulan', 'asal_new'], as_index=False).apply(lambda x: 
             pd.Series({'normal_kg':x.loc[x.kdproduk=='N']['berat'].sum(),
                         'urgent_kg':x.loc[x.kdproduk=='U']['berat'].sum(),
                         'top_urgent_kg':x.loc[x.kdproduk=='T']['berat'].sum(),
                         'darat_kg':x.loc[x.kdproduk=='D']['berat'].sum(),
-                        'trucking_kg':x.loc[x.kdproduk=='C']['berat'].count(),}))
+                        'trucking_kg':x.loc[x.kdproduk=='C']['kdproduk'].counta(),})).reset_index()
     
     lst_cab=kg_tujuan["asal_new"].drop_duplicates().sort_index(ascending=True)
     pilihan4=st.selectbox("Pilih Cabang", lst_cab, key="cabang")  
