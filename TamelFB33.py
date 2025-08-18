2a0dd285a63f74ced8f4f76551baf2d1b29ce657
widiantoko <dodiwidiantoko@gmail.com>
2025-08-18 19:10:37 +0700

    edit

--- TamelFB33.py
+++ TamelFB33.py
@@ -796,7 +796,7 @@ FROM
 
     with col2:
 
-            st.dataframe(datapage4[(datapage4.cabang==pil_cab)], hide_index=True, )
+            st.data_editor(datapage4[(datapage4.cabang==pil_cab)], hide_index=True, )
             #pil_cab=st.selectbox(label="**Pilih Cabang:**",options= lst_cab)
 
 
