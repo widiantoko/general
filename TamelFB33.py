264ea222f39c24b1ccce24ee19fa5f20dcb969a1
widiantoko <dodiwidiantoko@gmail.com>
2026-01-05 16:13:27 +0700

    oooo

--- TamelFB33.py
+++ TamelFB33.py
@@ -614,6 +614,7 @@ if selected2=="Volume Kiriman":
     from bokeh.models import ColumnDataSource
     from bokeh.plotting import figure, show
     from bokeh.transform import dodge
+    from bokeh.models import NumeralTickFormatter, FuncTickFormatter
 
     fruits = bulanku
     years = tahunku
@@ -628,19 +629,20 @@ if selected2=="Volume Kiriman":
     pgab = figure(x_range=fruits, y_range=(0, 200000), title="Volume Berat Kiriman per Bulan Tahun 2024 - 2026",
            height=350, width=1200, toolbar_location=None, tools="")
 
-    pgab.vbar(x=dodge('fruits', -0.25, range=pgab.x_range), top='2024', source=source,
+    pgab.vbar(x=dodge('fruits', -0.35, range=pgab.x_range), top='2024', source=source,
        width=0.2, color="#c9d9d3", legend_label="2024")
 
     pgab.vbar(x=dodge('fruits',  0.0,  range=pgab.x_range), top='2025', source=source,
        width=0.2, color="#718dbf", legend_label="2025")
 
-    pgab.vbar(x=dodge('fruits',  0.25, range=pgab.x_range), top='2026', source=source,
+    pgab.vbar(x=dodge('fruits',  0.35, range=pgab.x_range), top='2026', source=source,
        width=0.2, color="#e84d60", legend_label="2026")
 
     pgab.x_range.range_padding = 0.05
     pgab.xgrid.grid_line_color = None
     pgab.legend.location = "top_left"
     pgab.legend.orientation = "horizontal"
+    pgab.yaxis.formatter = NumeralTickFormatter(format="0,0")
     #pgab.axis_label_text_font_size = '10px'
 
     st.bokeh_chart(pgab)
