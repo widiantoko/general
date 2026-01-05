0dca64c178969bb68fdbd22f5ba3590cd526d2d0
widiantoko <dodiwidiantoko@gmail.com>
2026-01-05 11:00:47 +0700

    ui

--- TamelFB33.py
+++ TamelFB33.py
@@ -607,7 +607,7 @@ if selected2=="Volume Kiriman":
     #st.text(res_months)
 
 
-    st.title(datapage3[["berat_kg"]].astype(int))
+    st.markdown(datapage3[["berat_kg"]].astype(int))
     
     p4= datapage3.plot_bokeh(
         kind='bar',
