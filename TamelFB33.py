626847025a1a15699459a2173c114a6e3a5ddf5d
widiantoko <dodiwidiantoko@gmail.com>
2026-01-06 16:21:02 +0700

    999

--- TamelFB33.py
+++ TamelFB33.py
@@ -610,7 +610,7 @@ if selected2=="Volume Kiriman":
 
     #st.text(list_b)
 
-    df2024 = pd.read_csv('data/vol2024_2025_streamlit.csv')
+    df2024 = pd.read_csv('data/vol2024_2025_streamlit.csv', sep=',')
 
     st.text(df2024.info())
 
